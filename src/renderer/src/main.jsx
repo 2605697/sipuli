4704f3e24925397c3104c12d9c4369d4e305d3d3
Jip Hoogendam <hoogendam.jip@gmail.com>
2026-09-16 23:18:59 +0300

    Added basic project setup

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,19 @@
 import './assets/main.css'
+import electronLogo from './assets/electron.svg'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Post from './components/Post'
+import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div>
+      <Sidebar></Sidebar>
+      <Feed></Feed>
+      <Sidebar></Sidebar>
+    </div>
   </StrictMode>
 )
